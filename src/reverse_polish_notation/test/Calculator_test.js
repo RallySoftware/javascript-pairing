b780e2ac1545c240bd194225e98092ff3b7bb89b
Kurt Harriger <kharriger@rallydev.com>
2016-11-15 15:28:23 -0700

    add starter test for caclulator

---
+++ src/reverse_polish_notation/test/Calculator_test.js
@@ -0,0 +1,13 @@
+import { Calculator } from '../Calculator';
+
+describe("Calculator", function() {
+  let calculator =
+  beforeEach(() => {
+    calculator = new Calculator();
+  });
+
+  it('is created', () => {
+    expect(calculator).to.exist;
+  });
+
+});
