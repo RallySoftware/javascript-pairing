f3007432f047c374607ebc008be0c3e4f02093af
Kurt Harriger <kharriger@rallydev.com>
2016-11-15 16:03:33 -0700

    optionally use jasmine

---
+++ src/reverse_polish_notation/spec/Calculator.spec.js
@@ -0,0 +1,12 @@
+import { Calculator } from '../Calculator';
+
+describe("Calculator", function() {
+  let calculator;
+  beforeEach(() => {
+    calculator = new Calculator();
+  });
+
+  it('is created', () => {
+    expect(calculator).toBeDefined();
+  });
+});
